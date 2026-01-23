1d90fbe671b59ad37e043e3ae150b3bcbb1259da
Brittani Lovelace <blovelace84@gmail.com>
2026-01-22 21:47:51 -0500

    Delete

--- yardsale/app/(auth)/magic-link/page.tsx
+++
@@ -1,9 +0,0 @@
-import MagicLinkForm from "@/components/auth/MagicLinkForm";
-
-export default function MagicLinkPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <MagicLinkForm />
-    </div>
-  );
-}
